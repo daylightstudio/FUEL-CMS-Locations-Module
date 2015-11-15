d0c8e6b59ec41f5d34d62bf21a337f6afd5fd711
David McReynolds <dave@bydaylight.com>
2015-11-15 13:36:14 -0800

    Added location_map helper function

--- helpers/locations_helper.php
+++ helpers/locations_helper.php
@@ -1,1 +1,13 @@
 <?php  // put your helper functions here
+
+if ( ! function_exists('location_map'))
+{
+	function location_map($width = '100%', $height = 500, $zoom = 13, $id = 'mapiframe')
+	{
+		if (is_array($width))
+		{
+			extract($width);
+		}
+		return '<iframe src="'.site_url('locations/map?zoom='.$zoom).'" width="'.$width.'" height="'.$height.'" frameborder="0" style="border:0" class="map" name="mapiframe" id="'.$id.'"></iframe>';
+	}
+}
