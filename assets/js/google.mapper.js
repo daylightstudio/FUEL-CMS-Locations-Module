b3f9c53ab9e86d879ae8f1db2a8c29dd18da4584
David McReynolds <dave@bydaylight.com>
2015-11-16 10:36:03 -0800

    Added options for styles

--- assets/js/google.mapper.js
+++ assets/js/google.mapper.js
@@ -121,7 +121,7 @@ GoogleMapper.prototype = {
 			    opened: true
 			  },
 			//styles: [ {"featureType": "poi", "stylers": [{"visibility": "simplified"} ] }, {"featureType": "road", "stylers": [{"visibility": "simplified"} ] }, {"featureType": "water", "stylers": [{"visibility": "simplified"} ] }, {"featureType": "transit", "stylers": [{"visibility": "simplified"} ] }, {"featureType": "landscape", "stylers": [{"visibility": "simplified"} ] }, {"featureType": "road.highway", "stylers": [{"visibility": "off"} ] }, {"featureType": "road.local", "stylers": [{"visibility": "on"} ] }, {"featureType": "road.highway", "elementType": "geometry", "stylers": [{"visibility": "on"} ] }, {"featureType": "water", "stylers": [{"color": "#84afa3"}, {"lightness": 52 } ] }, {"stylers": [{"saturation": -77 } ] }, {"featureType": "road"} ]
-			styles: [ ]
+			styles: this.options.styles
 			};
 			
 		//mapOptions.center = results[0].geometry.location;
